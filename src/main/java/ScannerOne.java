e504824c6285cd2b9916c1e8bb483a7a41789466
Viktar Maroz <vmarozmail@gmail.com>
2022-12-28 19:19:26 +0100

    Added two new classes: ScannerOne and WhatIsYourName

--- src/main/java/ScannerOne.java
+++ src/main/java/ScannerOne.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ScannerOne {
+import java.util.Scanner;
+
+public class ScannerOne {
+    public static void main (String[] args){
+
+        Scanner enter = new Scanner("Once, upon a time...\n" + "Far, far away...\n");
+        String one = enter.nextLine();
+        System.out.println(one);
+        one = enter.nextLine();
+        System.out.println(one + "\n" + "********************");
+    }
 }
---
+++ src/main/java/WhatIsYourName.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class WhatIsYourName {
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+
+
+        if (!keyboard.hasNextInt()){
+            String name = keyboard.nextLine();
+            String nameUp = name.toUpperCase();
+            System.out.println("Nice to meet you, "+ nameUp);
+        }
+        else {
+            System.out.println("It looks as if You are entering numbers. Please enter letters");
+            String name = keyboard.nextLine();
+        }
+        //need to record the variable nameUP for the next iteration
+        //String name = keyboard.nextLine();
+        //String nameUp = name.toUpperCase();
+
+
+
+        System.out.println("Please enter your surname:");
+        if (!keyboard.hasNextInt()){
+            String surname = keyboard.nextLine();
+            String surnameUp = surname.toUpperCase();
+            System.out.println("Now we've got a bit closer, " + " " + surnameUp);
+        }
+        else {
+            String surname = keyboard.nextLine();
+            System.out.println("It looks as if You are entering numbers. Please enter letters");
+            System.out.println("Please enter your surname:");
+
+        }
+    }
+}
